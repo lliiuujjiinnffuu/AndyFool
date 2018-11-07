e66bd90c62d87a52f4da0a69a6625ac8beeb07c4
Liu Jinfu <liujinfu123321@foxmail.com>
2018-11-07 23:05:44 +0800

    netcorewebtest1

---
+++ WebApplication1/controller/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wxminigame.server
+{
+    [Route("/file")]
+    public class FileController : ControllerBase
+    {
+        [Route("test/{parameter}")]
+        public string Name()
+        {
+            return "AndyFool's, 福的测试";
+        }
+
+    }
+}
